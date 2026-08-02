451ea8c84e9104831702ee917957a3f0f0ad4cb6
JOSE DANIEL GARCIA PINEDA <jose.gpineda@mail.escuelaing.edu.co>
2026-08-02 12:06:00 -0500

    feat: first implementation challenge 2

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -1,5 +1,13 @@
 package challenge2;
+import java.util.function.Function;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ParallelRace {
-    
+    public Function<List<Integer>,Integer> maximum = l -> Collections.max(l);
+
+    public void firstCollition(){
+        System.out.println(maximum.apply(new ArrayList<Integer>(List.of(1,2,3,56,2,1))));
+    }
 }
