b3e4b1552d4ac8446cbffcd4b6694ea9bf5f38fd
Thomas Garcia <thomassebastiangarciagomez@gmail.com>
2026-08-02 16:16:52 -0500

    Refactor of code

--- Laboratory1/challenge1/Challenge1.java
+++ Laboratory1/challenge1/Challenge1.java
@@ -1,10 +1,4 @@
 package challenge1;
-
-/**
- * This is a main class of the challenge 1 from laboratory 1
- * 
- */
-
 public class Challenge1{
     public static void main(String[] args){
         WelcomeMessage wm = new WelcomeMessage();
---
+++ Laboratory1/challenge6/Challenge6.java
@@ -0,0 +1,20 @@
+package challenge6;
+
+public class Challenge6 {
+    
+    public Challenge6(){}
+
+    /**
+     * 
+     * @param command
+     * @return
+     */
+    public String executeComand(String command){
+        return "";
+    }
+
+    
+    public static void main(String[] args){
+
+    }
+}
