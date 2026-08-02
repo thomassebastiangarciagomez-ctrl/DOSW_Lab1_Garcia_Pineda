a732b9d1f5b0927ee111810a338920a103e227de
Jose Daniel Garcia Pineda <164280912+KenjiMaster@users.noreply.github.com>
2026-08-02 10:50:22 -0500

    Revert "Feature/challenge 1 garcia jose 2026 2"

--- Laboratory1/challenge1/Challenge1.java
+++
@@ -1,7 +0,0 @@
-package Laboratory1.challenge1;
-public class Challenge1{
-    public static void main(String[] args){
-        WelcomeMessage wm = new WelcomeMessage();
-        wm.printMessage();
-    }
-}
