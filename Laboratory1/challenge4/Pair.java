1e52b805358162bd888146eda2c625b58c6c2b73
Thomas Garcia <thomassebastiangarciagomez@gmail.com>
2026-08-02 19:05:34 -0500

    feat: challenge 4 completed

---
+++ Laboratory1/challenge4/Pair.java
@@ -0,0 +1,16 @@
+package challenge4;
+
+public class Pair {
+    private String first;
+    private Integer second;
+    public Pair(String first,Integer second){
+        this.first = first;
+        this.second = second;
+    }
+    public String getF(){
+        return first;
+    }
+    public Integer getS(){
+        return second;
+    }
+}
